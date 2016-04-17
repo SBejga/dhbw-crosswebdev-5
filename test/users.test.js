bfb49e06dafa37144e23618445d7c233b4bd85d5
Sebastian Bejga <sebastian@bejga.de>
2016-04-17 19:28:56 +0200

    update users test

--- test/users.test.js
+++ test/users.test.js
@@ -6,10 +6,42 @@ var assert = require('assert');
  * Test /Users
  */
 
-describe('POST /users', function(){
-    it('should respond with json', function(done){
+describe('GET /users', function(){
+    it('should be >= 0 users', function(done){
+        request(app)
+            .get('/users')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res){
+                if (err) done(err);
 
-        var newUser = {name: "test"};
+                assert.equal(res.body.length >= 0, true, ">= 0 users");
+
+                done();
+            });
+    })
+});
+
+describe('GET /users/:id', function(){
+    it('should not found user', function(done){
+
+        var id = "000000000000000000000000";
+
+        request(app)
+            .get('/users/' + id)
+            .expect('Content-Type', /json/)
+            .expect(404, done);
+    })
+});
+
+describe('Create User, Get and Update', function(){
+
+    var underwood;
+    var underwood2;
+
+    it('should create User', function(done){
+
+        var newUser = {firstname: "Frank", lastname: "Underwood", nick: "president_"+Date.now(), admin: true};
 
         request(app)
             .post('/users')
@@ -19,49 +51,41 @@ describe('POST /users', function(){
             .end(function(err, res){
                 if (err) done(err);
 
-                assert.equal(res.body.name, newUser.name, "created name should be match");
+                assert.equal(res.body.firstname, newUser.firstname, "created name should be match");
+                assert.equal(res.body.lastname, newUser.lastname, "created name should be match");
+                assert.equal(res.body.nick, newUser.nick, "created nick should be match");
+                assert.equal(res.body.admin, newUser.admin, "created user should be admin");
+
+                assert.notEqual(res.body._id, false, "created user should have _id");
+
+                underwood = res.body;
 
                 done();
             });
-    })
-});
+    });
 
-describe('GET /users', function(){
-    it('should respond with json', function(done){
-        request(app)
-            .get('/users')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .end(function(err, res){
-                if (err) done(err);
-                done();
-            });
-    })
-});
-
-describe('GET /users/:id', function(){
-    it('should respond with json', function(done){
-
-        var id = "42";
+    it('should get created user', function(done){
 
         request(app)
-            .get('/users/'+id)
+            .get('/users/'+underwood._id)
             .expect('Content-Type', /json/)
             .expect(200)
             .end(function(err, res){
                 if (err) done(err);
 
-                assert.equal(res.body._id, id, "id should match");
+                assert.equal(res.body.firstname, underwood.firstname, "created name should be match");
+                assert.equal(res.body.lastname, underwood.lastname, "created name should be match");
+                assert.equal(res.body.nick, underwood.nick, "created nick should be match");
+
+                assert.equal(res.body._id, underwood._id, "created user _id should match");
 
                 done();
             });
-    })
-});
+    });
 
-describe('PUT /users', function(){
-    it('should respond with json', function(done){
+    it('should update created user', function(done){
 
-        var putUser = {_id: "42", name: "test"};
+        var putUser = {_id: underwood._id, firstname: "Claire"};
 
         request(app)
             .put('/users/')
@@ -72,29 +96,41 @@ describe('PUT /users', function(){
                 if (err) done(err);
 
                 assert.equal(res.body._id, putUser._id, "id should match");
-                assert.equal(res.body.name, putUser.name, "name should match");
+                assert.equal(res.body.firstname, putUser.firstname, "name should match");
+
+                underwood2 = res.body;
 
                 done();
             });
-    })
-});
+    });
 
-describe('DELETE /users', function(){
-    it('should respond with json', function(done){
-
-        var deleteUser = {_id: "42"};
+    it('should get updated user', function(done){
 
         request(app)
-            .delete('/users/')
-            .send(deleteUser)
+            .get('/users/'+underwood._id)
             .expect('Content-Type', /json/)
             .expect(200)
             .end(function(err, res){
                 if (err) done(err);
 
-                assert.equal(res.body._id, deleteUser._id, "id should match");
+                assert.equal(res.body.firstname, underwood2.firstname, "created name should be match");
+                assert.equal(res.body.lastname, underwood2.lastname, "created name should be match");
+                assert.equal(res.body.nick, underwood2.nick, "created nick should be match");
+
+                assert.equal(res.body._id, underwood2._id, "created user _id should match");
 
                 done();
             });
+    });
+});
+
+describe('DELETE /users', function(){
+    it('should be 501', function(done){
+
+        request(app)
+            .delete('/users/')
+            // .send(deleteUser)
+            .expect('Content-Type', /json/)
+            .expect(501, done);
     })
 });
