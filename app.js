fca3285d82378379226bde04fb2d64a72f4b635a
Sebastian Bejga <sebastian@bejga.de>
2016-04-17 17:26:48 +0200

    reformat comments

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
 var express = require('express');
 var app = express();
 
-/**
- * Server Config
- */
+// ### Server Config
 var port = process.env.PORT || 3000; //use 3000 or environment var
 
-/**
- * Express Config
- */
+// ### Express Config
 var bodyParser = require('body-parser');
-
 app.use(bodyParser.json());
 
-/**
- * Routes
- */
-
+// ### Routes
 // route /users
 var usersRoute = require('./routes/users.js');
-
 app.use('/users', usersRoute);
 
 // public route for static files (frontend)
 app.use(express.static('public'));
 
-/**
- * Startup
- */
+// ### Startup
 app.listen(port, function (err) {
 	if (err) {
 		console.error("express server failed: ", err);
--- routes/users.js
+++ routes/users.js
@@ -1,63 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
-
-/**
- * POST new User
- */
+// POST new User
 router.post('/', function (req, res) {
     var body = req.body;
-
     //TODO: create new user
     body._id = "42";
-
     res.status(200).send(body);
 });
 
 
-/**
- * GET all Users
- */
+// GET all Users
 router.get('/', function (req, res) {
-    
     //TODO: send array of users without credentials
     res.status(200).send([{_id: ""}]);
-    
 });
 
-/**
- * GET one user by id
- */
+//GET one user by id
 router.get('/:id', function (req, res) {
-
     var id = req.params.id;
-
     //TODO: Switch between two cases:
     // foreign access of user details (filtered fields)
     // own access of user details (complete fields)
-
     res.send({_id: id});
 });
 
-/**
- * PUT update one user
- */
+//PUT update one user
 router.put('/', function (req, res) {
     var user = req.body;
-
     //TODO: update user if allowed
-    
     res.status(200).send(user);
 });
 
-/**
- * DELETE delete User
- */
+//DELETE delete User
 router.delete('/', function (req, res) {
     var body = req.body;
-
     //TODO: delete user if allowed
-
     res.status(200).send(body);
 });
 
